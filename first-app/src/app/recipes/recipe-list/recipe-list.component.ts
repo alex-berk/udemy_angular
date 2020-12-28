8ec5a151448641bc97a0077df03ce00948257aaa
Alex Berkovich <alexb@tastewise.io>
2020-12-28 20:26:49 +0200

    #cp adding a recipe model

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('New Recipe', 'Simple recipe of a simple dish',
+    'https://www.sweetrecipeas.com/wp-content/uploads/2020/03/Chocolate-Sprinkle-Yeast-Donuts-03.jpeg')
+  ];
 
   constructor() { }
 
