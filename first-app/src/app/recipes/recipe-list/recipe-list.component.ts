1a0a5dc1c1cb13682cd28d6b9c085835c890e883
Alex Berkovich <alexb@tastewise.io>
2021-01-01 15:37:25 +0200

    #cp added recipe details

--- first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('New Recipe', 'Simple recipe of a simple dish',
-    'https://www.sweetrecipeas.com/wp-content/uploads/2020/03/Chocolate-Sprinkle-Yeast-Donuts-03.jpeg')
+    'https://www.sweetrecipeas.com/wp-content/uploads/2020/03/Chocolate-Sprinkle-Yeast-Donuts-03.jpeg'),
+    new Recipe('New Recipe 2', 'Another simple recipe of a simple dish',
+    'https://www.drinksupermarket.com/media/catalog/product/cache/1/image/600x/9df78eab33525d08d6e5fb8d27136e95/c/o/coca-cola-original-330ml.jpg')
   ];
 
   constructor() { }
