5886eca27ced0b7fd820161701eb50fc3e0bd79b
Alex Berkovich <alexb@tastewise.io>
2021-01-19 23:04:10 +0200

    #cp made possible to move ingredients from recipe to shopping list

--- first-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ first-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,7 +10,11 @@ import { Recipe } from '../recipe.model'
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private spList: ShoppingListService) { }
+
+  toShoppingList() {
+    this.spList.addIngredientsBulk(this.recipe.ingredients);
+  }
 
   ngOnInit() {
   }
